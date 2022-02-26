1c90699f146c1fbefa5c4d7d9b67a75ea5c5d7f9
Nathan Wallis <natewallis@gmail.com>
2022-02-27 08:11:27 +1100

    tests edge retrieval

--- src/Traverse/AdjacencyMatrix.php
+++ src/Traverse/AdjacencyMatrix.php
@@ -15,8 +15,12 @@ class AdjacencyMatrix{
         $this->store[$to][$from] = $weight;
     }
 
-    public function get_edge($from, $to) {
+    public function get_edge_weight($from, $to) {
         return $this->store[$from][$to] ?? false;
+    }
+
+    public function get_connected_nodes($node) {
+        return array_keys($this->store[$node]);
     }
 
 }
--- tests/AdjacencyMatrixTest.php
+++ tests/AdjacencyMatrixTest.php
@@ -7,14 +7,14 @@ use Traverse\AdjacencyMatrix;
 
 final class AdjacencyMatrixTest extends TestCase {
 
-    public function testAddEdge():void{
+    public function test_add_edge():void{
 
         $test_latency = 666;
         $matrix = new AdjacencyMatrix();
         $matrix->add_weighted_edge('A','D',$test_latency);
 
-        $first_direction_latency = $matrix->get_edge('A','D');
-        $second_direction_latency = $matrix->get_edge('D','A');
+        $first_direction_latency = $matrix->get_edge_weight('A','D');
+        $second_direction_latency = $matrix->get_edge_weight('D','A');
 
         $this->assertEquals(
             $first_direction_latency,
@@ -26,6 +26,20 @@ final class AdjacencyMatrixTest extends TestCase {
             $test_latency
         );
 
+    }
+
+    public function test_edge_retrieval():void{
+
+        $matrix = new AdjacencyMatrix();
+        $matrix->add_weighted_edge('A', 'C', 100);
+        $matrix->add_weighted_edge('A', 'F', 200);
+        $matrix->add_weighted_edge('F', 'F', 10);
+        $linked_nodes_a = $matrix->get_connected_nodes('A');
+        $linked_nodes_f = $matrix->get_connected_nodes('F');
+      
+        $this->assertEqualsCanonicalizing($linked_nodes_a, ['C','F']);
+        $this->assertEqualsCanonicalizing($linked_nodes_f, ['A','F']);
+    
     }
 
 }
