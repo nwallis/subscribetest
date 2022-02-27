7d38e9ec34cf3eeefe066b8116687e0d3c3842b5
Nathan Wallis <natewallis@gmail.com>
2022-02-27 20:22:33 +1100

    adds test for valid path search

--- tests/AdjacencyMatrixTest.php
+++ tests/AdjacencyMatrixTest.php
@@ -29,17 +29,26 @@ final class AdjacencyMatrixTest extends TestCase {
     }
 
     public function test_edge_retrieval():void{
-
         $matrix = new AdjacencyMatrix();
         $matrix->add_weighted_edge('A', 'C', 100);
         $matrix->add_weighted_edge('A', 'F', 200);
         $matrix->add_weighted_edge('F', 'F', 10);
-        $linked_nodes_a = $matrix->get_connected_nodes('A');
-        $linked_nodes_f = $matrix->get_connected_nodes('F');
-      
+        $linked_nodes_a = $matrix->get_adjacent_nodes('A');
+        $linked_nodes_f = $matrix->get_adjacent_nodes('F');
         $this->assertEqualsCanonicalizing($linked_nodes_a, ['C','F']);
         $this->assertEqualsCanonicalizing($linked_nodes_f, ['A','F']);
-    
+    }
+
+    public function test_valid_path_search():void{
+        $matrix = new AdjacencyMatrix();
+        $matrix->add_weighted_edge('A', 'B', 10);
+        $matrix->add_weighted_edge('A', 'C', 20);
+        $matrix->add_weighted_edge('B', 'D', 100);
+        $matrix->add_weighted_edge('C', 'D', 100);
+        $matrix->add_weighted_edge('D', 'E', 10);
+        $matrix->add_weighted_edge('E', 'F', 1000);
+        [$path_found, $latency_accumulator, $visited_accumulator] = $matrix->find_acceptable_path('A','F',1200);
+        $this->assertEquals($path_found, true);
     }
 
 }
