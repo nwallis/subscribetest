9ebda41f1863eadd84dce290c7eea196fc4d1587
Nathan Wallis <natewallis@gmail.com>
2022-02-28 03:39:48 +1100

    adds path search implementation

--- src/Traverse/AdjacencyMatrix.php
+++ src/Traverse/AdjacencyMatrix.php
@@ -16,11 +16,44 @@ class AdjacencyMatrix{
     }
 
     public function get_edge_weight($from, $to) {
-        return $this->store[$from][$to] ?? false;
+        return $this->store[$from][$to] ?? 0;
     }
 
-    public function get_connected_nodes($node) {
+    public function get_adjacent_nodes($node) {
         return array_keys($this->store[$node]);
+    }
+
+    public function get_unvisited_nodes($node, $visited){
+        return array_diff($this->get_adjacent_nodes($node), $visited);
+    }
+
+    public function find_fast_enough_path($from, $to, $max_latency, $visited = [], $latency = [], $path = []){
+
+        $visited[] = $from;
+        $path[] = $from;
+        
+        if (array_sum($latency) > $max_latency) return [$visited, [], []];
+
+        if ($from === $to) {
+            if (array_sum($latency) <= $max_latency){
+                return [$visited, $latency, $path];
+            }else{
+                return [$visited, [], []];
+            }
+        }
+
+        $unvisited_nodes = $this->get_unvisited_nodes($from, $visited);
+        if (empty($unvisited_nodes)) return [$visited, [], []];
+
+        foreach($unvisited_nodes as $adjacent_node){
+            $latency[] = $this->get_edge_weight($from,$adjacent_node);
+            [$visited_b, $latency_b, $path_b] = $this->find_fast_enough_path($adjacent_node, $to, $max_latency, $visited, $latency, $path);
+            if (!empty($latency_b) && !empty($path_b)) return [$visited_b, $latency_b, $path_b];
+            array_pop($latency);
+        }
+
+        return [$visited, [], []];
+
     }
 
 }
